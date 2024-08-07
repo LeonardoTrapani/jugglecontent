3a391a9930a067761033e71e389ee6e10f5c6ac0
LeonardoTrapani <leonard.trapani@gmail.com>
2024-08-07 20:32:03 +0200

    fix: update with new domain

--- config/site.ts
+++ config/site.ts
@@ -7,7 +7,7 @@ export const siteConfig: SiteConfig = {
     "Go from one piece of content to 15. Juggle Content is a content creation tool that helps you create multiple pieces of content from one.",
   url: "https://jugglecontent.com",
   ogImage: absoluteUrl("/og.jpg"),
-  email: "jugglecontent@thecloserfit.com",
+  email: "info@jugglecontent.com",
   github: "https://github.com/leonardotrapani/jugglecontent",
   keywords: [
     "content creation",
