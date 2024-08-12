240694a0ba185e2cbe27f636c730b12f10bf4cf9
LeonardoTrapani <leonard.trapani@gmail.com>
2024-08-12 21:02:59 +0200

    fix: privacy link to /privacy

--- components/policies/privacy.tsx
+++ components/policies/privacy.tsx
@@ -1,18 +1,14 @@
-import Script from "next/script"
+import Link from "next/link"
 
 export default function PrivacyButton() {
   return (
     <>
-      <a
-        href="https://www.iubenda.com/privacy-policy/31416509"
-        className="iubenda-white iubenda-noiframe iubenda-embed iubenda-noiframe "
-        title="Privacy Policy "
+      <Link
+        href="/privacy"
+        className="hover:text-brand underline underline-offset-4"
       >
         Privacy Policy
-      </a>
-      <Script type="text/javascript" id="iubenda">
-        {`(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);`}
-      </Script>
+      </Link>
     </>
   )
 }
